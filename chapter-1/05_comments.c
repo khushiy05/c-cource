e5ffd2ad522dd7d21afd424a69a53d1adf164f12
khushiy05 <khushiyewale150@gmail.com>
2025-09-02 01:23:22 +0530

    comments

---
+++ chapter-1/05_comments.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    // this is called multi-line comments
+    /* this is single-line comments*/
+    // for any heading for info gcc will not print this
+    return 0;
+}
