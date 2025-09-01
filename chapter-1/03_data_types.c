20d82f1938a4eb53ceb56d3994d4f15cee9e89bb
khushiy05 <khushiyewale150@gmail.com>
2025-09-02 01:30:19 +0530

    data types

--- chapter-1/03_data_types.c
+++ chapter-1/03_data_types.c
@@ -10,6 +10,11 @@ int main(){
 
     // char c = 'a';
     char c;
-    c ='a';
+    c ='a'; 
+
+    printf("The value of a is %d\n", a);
+    printf("The value of b is %d\n", b);
+    printf("The value of c is %d\n", c);
+
 
 }
