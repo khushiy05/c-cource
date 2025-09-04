fc1c0c442c6cf7980352ff4d78c323af9f1aade0
khushiy05 <khushiyewale150@gmail.com>
2025-09-04 21:38:44 +0530

    volume of cynlinder and area of circle

--- chapter-1-practice-set/02_problem2.c
+++ chapter-1-practice-set/02_problem2.c
@@ -2,11 +2,16 @@
 
 int main(){
     /*
-    calculate the area of a circle and modify the same program to the calculator the volume od a cylinder given its radius and height.
+    calculate the area of a circle and modify the same program to the calculator the volume of a cylinder given its radius and height.
     area of circle
     A=πr2
+    volume of a cylinder
+    V=πr2h
+    
     */
     int r = 6;
-    printf("The area of circle with the radius %d is %f", r, 3.14*r*r);
+    int height = 10;
+    printf("The area of circle with the radius %d is %f\n", r, 3.14*r*r);
+    printf("The volume of a cylinder with the radius %d and height %d is %f", r, 3.14*r*r*height);
     return 0;
 }
