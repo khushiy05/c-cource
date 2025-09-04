5d9f47ca08281836f442a7b31b06ae2e4e7c969d
khushiy05 <khushiyewale150@gmail.com>
2025-09-04 22:51:15 +0530

    simple interest

---
+++ chapter-1-practice-set/04_problem4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    /*write a peogram to calculate simple interest for a set of values representing principale, number of years and rate of interest*/
+    int p= 34;
+    int r= 8;
+    int t= 5;
+    printf("The value of simple interest is %f ",(p*r*t)/100);
+    return 0;
+}
