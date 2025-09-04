93e9d93ac1b646d3c074309a4529c5411d364dbe
khushiy05 <khushiyewale150@gmail.com>
2025-09-04 21:29:12 +0530

    area of circle

---
+++ chapter-1-practice-set/02_problem2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    /*
+    calculate the area of a circle and modify the same program to the calculator the volume od a cylinder given its radius and height.
+    area of circle
+    A=πr2
+    */
+    int r = 6;
+    printf("The area pf circle with the radius %d is %f", r, 3.14*r*r);
+    return 0;
+}
