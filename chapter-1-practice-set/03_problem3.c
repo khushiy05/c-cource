0391a18ec2d87ef564c465a4c6b2c4f27dd01359
khushiy05 <khushiyewale150@gmail.com>
2025-09-04 22:39:23 +0530

    c to f

---
+++ chapter-1-practice-set/03_problem3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    /*
+    write a program to convert celsius to fahrenheit*/
+    float c=37.0, f;
+    f = (9.0/5.0)*c + 32;
+    printf("The value in fahrenheit is %f", f);
+
+    return 0;
+}
