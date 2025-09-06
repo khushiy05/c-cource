475d0b347fc62c2d76b9d902ddf98ed7a3649931
khushiy05 <khushiyewale150@gmail.com>
2025-09-06 21:20:12 +0530

    typecasting

---
+++ chapter-2/05_typecasting.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int n = 45;
+    float m = 32.23;
+    // to convert float m into int
+    n =(int) m; //--> this is typecasting method
+    printf("%d\n", n);
+    return 0;
+}
