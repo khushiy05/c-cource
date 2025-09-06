1e70ee033997eecbf924403752c063c11fed4b8b
khushiy05 <khushiyewale150@gmail.com>
2025-09-06 21:50:27 +0530

    problem

---
+++ chapter-2-practice-set/03_problem3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    // write a program to check whether a number is diviible by 97 or not.
+    //int a = 2342354; // output will br 95 
+    int a = 3349895; // output will be 0
+    printf("the value of a%97 is %d",a%97);
+    return 0;
+}
