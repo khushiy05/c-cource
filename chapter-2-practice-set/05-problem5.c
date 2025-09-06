0b83a2b0116118574d3063e62c902a8580341226
khushiy05 <khushiyewale150@gmail.com>
2025-09-06 22:10:08 +0530

    problem

---
+++ chapter-2-practice-set/05-problem5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    /*
+    3.0+1 will be
+    a. integer
+    b. floating point number
+    c. character
+    */
+   float a = 3.0+1;
+   printf("The value of a is %f",a);
+   return 0;
+}
