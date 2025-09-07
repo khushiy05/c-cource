b4819d39e8f0d1a2be005661c5b9ed8489cbe0f3
khushiy05 <khushiyewale150@gmail.com>
2025-09-07 23:46:53 +0530

    if-else

---
+++ chapter-3/01_conditional.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int age = 15;
+    if(age>10){
+        printf("We are inside if\n");
+        printf("Your age is grater than 10\n");
+    }
+    return 0;
+}
