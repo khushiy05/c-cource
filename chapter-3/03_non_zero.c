173e91f89d3be030d197d84c775def6b2dad880d
khushiy05 <khushiyewale150@gmail.com>
2025-09-08 00:14:34 +0530

    non zero

---
+++ chapter-3/03_non_zero.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    if(1){
+        printf("This if is executed!\n");
+    }
+    if(2345){
+        printf("This if is also executed!\n");
+    }
+    if(2.74){
+        printf("This if is also executed!\n");
+    }
+    if('c'){
+        printf("This charcter inside if is also executed!\n");
+    }
+    if(0){
+        printf("I am zero -  I am not executed!\n");
+    }
+    return 0;
+}
