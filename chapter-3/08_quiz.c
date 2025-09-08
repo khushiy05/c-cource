fadb00f99a15e108ebfd0cd10e745a58d193bf9b
khushiy05 <khushiyewale150@gmail.com>
2025-09-08 23:35:11 +0530

    quiz

---
+++ chapter-3/08_quiz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(){
+    /*
+    quick quiz: weite a prigram to find grade of a student given his marks based on below:
+    90 - 100 => A
+    80 - 90 => B
+    70 - 80 => C
+    60 - 70 => D
+    50 - 60 => E
+    <50 => F
+    */
+   char grade;
+   int marks = 46;
+   if (marks<=100 && marks>=90)
+   {
+    grade = 'A';
+   }
+
+   else if(marks<=90 && marks>=80){
+    grade = 'B';
+   }
+
+   else if(marks<=90 && marks>=80){
+    grade = 'C';
+   }
+
+   else if(marks<=90 && marks>=80){
+    grade = 'D';
+   }
+
+   else if(marks<=90 && marks>=80){
+    grade = 'E';
+   }
+
+   else{
+    grade = 'F';
+   }
+   
+    return 0;
+}
