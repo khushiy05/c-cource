7d6ff7c26083c373c9d8a3cedc640fc60852b514
khushiy05 <khushiyewale150@gmail.com>
2025-09-22 22:53:22 +0530

    i-d

---
+++ chapter-4/increment_decrement.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int i = 5;
+    printf("The value of 1 is %d\n", i);
+
+    i = i +5; // 10
+    printf("The value of 1 is %d\n" , i);
+    i++; //11
+    printf("The value od i is %d\n" , i);
+    return 0;
+}
